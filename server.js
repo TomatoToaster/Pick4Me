996b5953a5b67889b1cde29cca644a274a161778
Amal Nazeem <amalnazeem@gmail.com>
2018-10-15 16:16:19 -0400

    Fixed logic for hard routes in server

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // Serves up both the express api and the react webapp on this server
 
-var express = require('express'); // call express
-var app = express(); // define our app using express
-var bodyParser = require('body-parser')
+const express = require('express'); // call express
+const app = express(); // define our app using express
+const bodyParser = require('body-parser');
+const path = require('path');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -11,18 +12,30 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/dist/front'))
 
 
-var api = require('./dist/api/api'); // import my bundled api
-var config = require('./Config');
+const api = require('./dist/api/api'); // import my bundled api
+const config = require('./Config');
 
 const port = process.env.PORT || config.port;      // set our port
+
+// Set up API
+// =============================================================================
 
 // Route all the calls prefixed with /api to the api we've imported
 app.use('/api', api);
 
-// Sends the front end application up
-app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname + '/dist/index.html'));
-})
+// Set up Front End
+// =============================================================================
+
+// This sends out the front end (index.html)
+const front_end_handler = function(request, response) {
+  response.sendFile(path.join(__dirname + '/dist/front/index.html'));
+}
+
+// These are the routes that should point to the front end
+const front_end_routes = ['/'];
+
+// Now we are sending the index.html to each of the front end routes
+front_end_routes.forEach(route => app.get(route, front_end_handler));
 
 // START THE SERVER
 // =============================================================================
